1502da3fa6220798ebb9f7611190d69f09a8a7a9
Dahuum <romaissaeabde@gmail.com>
2025-05-05 19:37:29 +0100

    Bmomentum

--- src/routes/v1/index.js
+++
@@ -1,8 +0,0 @@
-const authRoutes = require('./auth.routes');
-const userRoutes = require('./user.routes');
-
-module.exports = async function (fastify, opts) {
-  // Register all route groups
-  fastify.register(authRoutes, { prefix: '/auth' });
-  fastify.register(userRoutes, { prefix: '/users' });
-};
