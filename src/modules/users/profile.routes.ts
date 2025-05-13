86b1855f9cd7a1413143a148eb521b42bd142da1
Dahuum <romaissaeabde@gmail.com>
2025-05-13 13:52:23 +0100

    fix(profile): Improve username update reliability

--- src/modules/users/profile.routes.ts
+++ src/modules/users/profile.routes.ts
@@ -21,14 +21,12 @@ export default async function protectedRoutes(app: FastifyInstance) {
   /* <-- update username route --> */
   app.patch("/username", {preHandler: [app.authenticate] }, async (request, reply) => {
     const user: any = request.user;
-    console.log(request.body);
     const body = userSchema.safeParse(request.body);
-    console.log("t7awaaaaaaaaaa....")
     if (!body.success)
       return reply.status(400).send({ error: "invalide data" });
     
     const newUsername = body.data.username;
-    console.log("newUsername: " + newUsername);
+    
     try {
       const updateUser = await prisma.user.update({
         where: { id: user.id },
@@ -38,15 +36,11 @@ export default async function protectedRoutes(app: FastifyInstance) {
       });
       return reply.send({ user: updateUser });
     } catch (err: any) {
-          if (
-            err.code === "P2002" &&
-            err.meta?.target?.includes("username")
-          ) {
-            return reply.status(409).send({ error: "Username already taken" })
-          }
-    
-          return reply.status(500).send({ error: "Server error, (Brother is it from here)" })
-        }
+      console.log(err.meta); /* 4er bash nshuf wa7ed l9adia */
+        if ( err.code === "P2002" && err.meta?.target?.includes("username"))
+          return reply.status(409).send({ error: "Username already taken" })
+        return reply.status(500).send({ error: "Server error, (Brother is it from here)" })
+      }
   })
   /* <-- update username route --> */
   
