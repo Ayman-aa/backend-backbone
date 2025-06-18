3272d69744b5006cf321fda5518f5e7428a91868
Dahuum <romaissaeabde@gmail.com>
2025-06-18 23:23:11 +0100

    feat: Continue developing tournament features.

--- src/modules/game/game.tournaments.routes.ts
+++ src/modules/game/game.tournaments.routes.ts
@@ -1,11 +1,22 @@
-// import { FastifyInstance } from "fastify";
-// const bcrypt = require("bcrypt");
-// import { PrismaClient } from "@prisma/client";
-// import { prisma } from '../../utils/prisma';
+import { FastifyInstance } from "fastify";
+const bcrypt = require("bcrypt");
+import { PrismaClient } from "@prisma/client";
+import { prisma } from '../../utils/prisma';
+
+/* 
+  Tournament
+  ├── Basic Info (name, max players, status)
+  ├── Participants (who joined)
+  ├── Brackets (the matchups)
+  └── Games (actual matches played)
+  
+  Tournament
+  ├── Basic Info (name, max players, status)
+  ├── Participants (who joined)
+  ├── Brackets (the matchups)
+  └── Games (actual matches played)
+
+*/
 
 
-// export default async function tournament(app: FastifyInstance) {
-//   app.post("/create", { preHandler: [app.authenticate] }, async (req, reply) => {
-    
-//   })
-// }
+}
